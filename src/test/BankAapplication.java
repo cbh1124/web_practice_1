83642bf5668754d3f5009401d38bdb4172c94225
cbh1124 <119vkfks@gmail.com>
2021-10-07 02:07:01 +0900

    아 모르겠엉

--- src/test/BankAapplication.java
+++ src/test/BankAapplication.java
@@ -27,7 +27,7 @@ public class BankAapplication {
 			}
 			// 계좌 목록
 			else if(ch == 2) {
-				// 메소드 입력
+				listA();
 			}
 			// 예금
 			else if(ch == 3) {
@@ -40,12 +40,10 @@ public class BankAapplication {
 			// 종료 
 			else if(ch == 5) {
 				run = false;
-				System.out.println("시스템을 종료하셨습니다.");
-				
 			}
-			break;
+
 		}
-		
+		System.out.println("시스템을 종료하셨습니다.");
 	}
 	
 	
@@ -54,15 +52,15 @@ public class BankAapplication {
 		System.out.println("계좌 생성을 선택하셨습니다.");
 		System.out.println("-----------------------------------------");
 		
-		Account account = new Account(ago,owner,balance);
-		
 		System.out.println("계좌입력 : "); String ago = sc.next();
 		System.out.println("계좌주 : ");	String owner = sc.next();
-		System.out.println("초기 계좌 금액 입력"); int balance = sc.nextInt();
+		System.out.println("초기 계좌 금액 입력 : "); int balance = sc.nextInt();
 		// 초기 계좌 금액을 밸런스에 넣고 저장 
 		
+		Account account = new Account(ago,owner,balance);
+		
 		for(int i=0; i<ac.length;i++ ) {
-			if(ac == null) {
+			if(ac[i] == null) {
 				ac[i] = account;
 				System.out.println("계좌 생성 완료");
 				break;
@@ -71,15 +69,48 @@ public class BankAapplication {
 			
 	}
 	
-	private static void list() {
+	// 계좌목록보기 
+	// 계좌 리스트를 배열에서 반복문으로 다가져온다음에 
+	// null이 나오면 순서상 마지막 배열저장전의 값이기때문에 그때 정지
+	// null이 나오기전에는 배열 하나씩 출력 / 따라서 ac배열의 값을 null값이 나올때까지 돌린다음 배열 출력
+	private static void listA() {
 		System.out.println("-----------------------------------------");
 		System.out.println("계좌 리스트 출력");
 		System.out.println("-----------------------------------------");
 		
+		for(int i = 0; i<ac.length; i++) {
+			if(ac[i] == null) {
+				break;
+			}
+		System.out.println(ac[i].getAgo() +"\t" + ac[i].getOwner()+ "\t"+ ac[i].getBalance());		
+			
+		}
+	}
+	//??????????????????????????????????????????????????애매하군.. 
+	private static Account findAccount(String ago) {
 		
+	}
+	// 예금    
+	/*
+	 * 예금 -> 입력 : 계좌번호 예금액 -> 계좌번호 일치 확인 ->
+	 * */
+	private static void setA() {
+		System.out.println("-----------------------------------------");
+		System.out.println("예금");
+		System.out.println("-----------------------------------------");
+		
+		//입력
+		System.out.println("계좌번호를 입력하세요 : "); String.ago = sc.next();
+		System.out.println("예금액을 입력하세요 : "); String.owner = sc.next();
+		
+		// 2. 입력받은 계좌 번호 일치
+		for(i=0;i<ac.length;i++) {
+			
+		}
+	}
 		
 }
 	
 	
 
-}
+
