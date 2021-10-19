94f9a95acc4c8e37ae706ba196974b62546b6dae
최병호 <119vkfks@gmail.com>
2021-10-19 18:12:58 +0900

    안녕하세요

---
+++ src/com/ezen_cbh/day14/Member.java
@@ -0,0 +1,7 @@
+package com.ezen_cbh.day14;
+
+public class Member< T, A > { // 외부로부터 타입을 정해서 해당 클래스로 값이 들어옴 
+	T Name;
+	A phone;
+	// String -> int -> char 등 
+}
---
+++ src/com/ezen_cbh/day14/Student.java
@@ -0,0 +1,9 @@
+package com.ezen_cbh.day14;
+
+public class Student extends Person {
+	
+	
+	public Student(String name) {
+		super(name);
+	}
+}
