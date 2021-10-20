9eca5c0e8d08ac6977825592eeb060b8713c4f74
cbh1124 <119vkfks@gmail.com>
2021-10-21 00:21:24 +0900

    ㅎㅇ

--- src/연습/Gradecard_main.java
+++ src/연습/Gradecard_main.java
@@ -25,48 +25,56 @@ public class Gradecard_main {
 		// 설정 리스트 선언 : 학생 정보를 담는 클래스 
 		ArrayList<Gradecard> students = new ArrayList<>();
 		
+		
 		while(true) {
-			System.out.println("1.성적기입  2.성적조회");
-			int ch = sc.nextInt();
-			if(ch == 1) {
-				System.out.println("번호를 입력하세요!! >>>"); int numbers = sc.nextInt();
-				System.out.println("이름을 입력하세요 !! >>>"); String name = sc.next(); 
-				System.out.println("국어 점수를 입력하세요  >>>"); int kor = sc.nextInt();
-				System.out.println("영어 점수를 입력하세요 >>>"); int eng = sc.nextInt();
-				System.out.println("수학 점수를 입력하세요 >>>"); int math = sc.nextInt();
-				int sumpoint = kor + eng + math; 
-				int avg = sumpoint/3; 
-				int rank = 1;
-				
-				Gradecard student = new Gradecard(numbers, name, kor, eng, math,sumpoint,avg, rank);
-				students.add(student); System.out.println("성공적으로 성적기입 완료");
-				
-				for(int i = 0; i<students.size(); i++) {
-					for(int j = 0; j<students.size(); j++) {
-						if(students.get(i).getSumpoint() > students.get(j).getSumpoint()) {
-							rank++;
-							students.get(j).setRank(rank) ;
+			
+			try {
+				System.out.println("1.성적기입  2.성적조회");
+				int ch = sc.nextInt();
+				if(ch == 1) { 
+					System.out.println("번호를 입력하세요!! >>>"); int numbers = sc.nextInt();
+					System.out.println("이름을 입력하세요 !! >>>"); String name = sc.next(); 
+					System.out.println("국어 점수를 입력하세요  >>>"); int kor = sc.nextInt();
+					System.out.println("영어 점수를 입력하세요 >>>"); int eng = sc.nextInt();
+					System.out.println("수학 점수를 입력하세요 >>>"); int math = sc.nextInt();
+					int sumpoint = kor + eng + math; 
+					double avg = sumpoint/3.00; 
+					int rank = 1;
+					
+					Gradecard student = new Gradecard(numbers, name, kor, eng, math,sumpoint,avg, rank);
+					students.add(student); System.out.println("성공적으로 성적기입 완료");
+					
+					for(int i = 0; i<students.size(); i++) {
+						for(int j = 0; j<students.size(); j++) {
+							if(students.get(i).getSumpoint() < students.get(j).getSumpoint()) {
+								rank++;
+								students.get(i).setRank(rank) ;
+							}
 						}
-					}
+						rank = 1;
+					} 
 				}
-				rank = 1;
 				
-				 
-				 
-			}
-			if(ch == 2) {
-				System.out.println("----------------------------------------");
-				System.out.println("성적표 ");
-				System.out.println("----------------------------------------");
-				System.out.println("번호\t이름\t국어\t영어\t수학\t총점\t평균\t석차"        );
-				for(Gradecard temp : students) {
-					System.out.println(temp.getNumbers()+"\t"+temp.getName()+"\t"+ temp.getKor()
-					+"\t"+temp.getEng()+"\t"+temp.getMath()+"\t"+temp.getSumpoint()+"\t"+temp.getAvg()
-					+"\t"+temp.getRank());
+				if(ch == 2) {
+					System.out.println("------------------------------------------------------------");
+					System.out.println("\t\t\t"+"성 "+"\t"+"적"+"\t"+"표");
+					System.out.println("------------------------------------------------------------");
+					System.out.println("번호\t이름\t국어\t영어\t수학\t총점\t평균\t석차");
+					for(Gradecard temp : students) {
+						/*
+						System.out.println(temp.getNumbers()+"\t"+temp.getName()+"\t"+ temp.getKor()
+						+"\t"+temp.getEng()+"\t"+temp.getMath()+"\t"+temp.getSumpoint()+"\t"+temp.getAvg()
+						+"\t"+temp.getRank());*/
+						
+						System.out.printf("%d\t%s\t%d\t%d\t%d\t%d\t%.2f\t%d\n",temp.getNumbers(),temp.getName(),temp.getKor(),
+								temp.getEng(), temp.getMath(), temp.getSumpoint(), temp.getAvg(),temp.getRank() );
+					}
+					System.out.println("------------------------------------------------------------");
 				}
-				System.out.println("----------------------------------------");
+			}catch (Exception e) {
+				sc= new Scanner(System.in);
+				System.out.println("정수를 다시 입력해주세요"); 
 			}
-
 		}
 		
 		
