268ad24ccb20c3463c3cda49ce525e29d993ee38
최병호 <119vkfks@gmail.com>
2021-10-21 12:27:18 +0900

    10_21 오전

---
+++ src/com/ezen_cbh/day16/Day16_1.java
@@ -0,0 +1,23 @@
+package com.ezen_cbh.day16;
+	
+/*
+ * 클래스 설계
+ * Member 
+ * 		필드 : id,password, name, email, point 
+ * 		생성자 : 빈생성자, 전체생성자
+ * 		메소드 : get, set 메소드
+ * File
+ * 		필드 : filepath
+ * 		메소드 : save, load 
+ * MemberController 
+ * 		필드 : ArrayList<Member> memeberlist 
+ * 		메소드 : login, signup, forgotid, forgotpassword, info, update, delete 
+ * 
+ * [코드]
+ * 자바 프로젝트 생성 [JDK1.8]
+ * 
+ * 
+ * */
+public class Day16_1 {
+	
+}
